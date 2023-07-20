faeb10a5d4cfea5d600bd96177460acb218a4f46
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2023-07-20 22:16:24 +0545

    smooth scroll

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 document
   .querySelector(".nav-links-container")
   .addEventListener("click", function (e) {
-    console.log(e.target);
+    // console.log(e.target);
 
     //matching strategy
     if (e.target.classList.contains("nav-link")) {
@@ -17,4 +17,11 @@ document.querySelector(".download-cv").addEventListener("click", function () {
   alert(
     "📝 Whoops, no CV at the moment! But don't worry, I'm busy crafting one. Soon, my fabulous CV will be ready to impress employers! 🛠️🚀 Stay tuned for greatness! 😄"
   );
+});
+
+const nav_height = document.querySelector("header").offsetHeight;
+const sections = document.querySelectorAll(".scroll");
+
+sections.forEach((section) => {
+  section.style.scrollMarginTop = `${nav_height}px`;
 });
